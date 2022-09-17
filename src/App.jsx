48e79aa99696ed3fe7d441a25a493016d7df7699
EtishaJain17 <jainetisha0714>
2022-09-17 15:32:33 +0530

    Initial Commit of SwapKart Ecommerce Website...

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div  className='bg-red-500 text-3xl text-center mt-48'>!!Hello SwapKart!!</div>
+  )
+}
+
+export default App
