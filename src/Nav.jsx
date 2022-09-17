567fff5390a1c2a5d0e2eaebd3ce28734be5b10f
EtishaJain17 <jainetisha0714>
2022-09-17 17:01:04 +0530

    Added the logo in Nav section and added a customised min-height in tailwind.config.cjs

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import printImage from './print.svg'
 function Nav() {
   return (
-    <div>Nav</div>
+    <div className='box-border border-b py-4'>
+        <div className='box-border px-5 mx-auto min-h-70 flex items-center'>
+        <img src={printImage} />
+        </div>
+    </div>
   )
 }
 export default Nav;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    minHeight: {
+      '70': '70px',
+    },
     extend: {},
   },
   plugins: [],
