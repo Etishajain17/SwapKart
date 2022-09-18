4f313f7d34bd968b6d8294872cf6209d6c576008
EtishaJain17 <jainetisha0714>
2022-09-18 12:26:50 +0530

    Some more fixes in Navigation bar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    minHeight: {
-      '70': '70px',
+      minHeight: {
+        '70': '70px',
+      },
+      
+      colors: {
+        'primary': 'rgb(255, 81, 81)',
+        'grey' :'rgb(65, 81, 97)',
+        'bordercolor' :'rgba(0, 0, 0, 0.07)',
+         white: colors.white,
+         black : colors.black,
+      },
+      screens:{
+        'middle': '920px',
+      },
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
     },
-    
-    colors: {
-      'primary': 'rgb(255, 81, 81)',
-      'grey' :'rgb(65, 81, 97)',
-      'bordercolor' :'rgba(0, 0, 0, 0.07)',
-    },
-    screens:{
-      'middle': '920px',
-    },
-    extend: {},
   },
   plugins: [],
 }
